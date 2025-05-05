7330ec0f28b15763b9b7e060533cc8b6510c7ff4
Sebadalpra <Sebastiandalpra@gmail.com>
2025-05-05 20:49:27 -0300

    actualizacion repo

---
+++ D/productos.js
@@ -0,0 +1,30 @@
+let producto =
+    [
+        {
+        codigo_barra : "2222",
+        nombre : "Heladera",
+        precio : 150,
+        fecha_vencimiento : "2050-04-01"
+        },
+        {
+        codigo_barra : "2222",
+        nombre : "Microondas",
+        precio : 300,
+        fecha_vencimiento : "2040-02-01"
+        },
+        {
+        codigo_barra : "4444",
+        nombre : "TV",
+        precio : 225,
+        fecha_vencimiento : "2050-03-01"
+        },
+        {
+            codigo_barra : "5555",
+            nombre : "PC",
+            precio : 755,
+            fecha_vencimiento : "2050-03-01"
+        }
+    ]
+
+console.log(producto)
+
